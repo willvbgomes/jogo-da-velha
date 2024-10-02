e7903ddf05ad75c1d3399a45b6dd65801daf735f
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-10-02 20:08:41 -0300

    update app

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,10 @@
-export const App = () => <></>
+import { Board } from './components/board'
+import { GameProvider } from './contexts/game-provider'
+
+export const App = () => (
+  <>
+    <GameProvider>
+      <Board />
+    </GameProvider>
+  </>
+)
