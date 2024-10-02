a0f19d864ee3fe0a940b6afdaa55743a0b8c0e14
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-10-02 20:02:08 -0300

    add board-row component

---
+++ src/components/board-row.tsx
@@ -0,0 +1,28 @@
+import { useGameContext } from '../hooks/game-context'
+import { Square } from './square'
+
+type BoardRowProps = {
+  rowIndex: number
+}
+
+export const BoardRow = ({ rowIndex }: BoardRowProps) => {
+  const { squares, setSquareValue } = useGameContext()
+
+  return (
+    <div className="flex border-t border-transparent [&+div]:border-t-zinc-100">
+      {Array(3)
+        .fill(null)
+        .map((_, i) => {
+          const squareIndex = rowIndex * 3 + i
+
+          return (
+            <Square
+              key={squareIndex}
+              value={squares[squareIndex]}
+              onClick={() => setSquareValue(squareIndex)}
+            />
+          )
+        })}
+    </div>
+  )
+}
