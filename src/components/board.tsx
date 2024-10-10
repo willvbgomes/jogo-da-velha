9f284bc861943feb2bb6a1974aae053b14af6a99
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-10-09 23:15:45 -0300

    remove some tailwind classes from board component

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,7 +1,7 @@
 import { BoardRow } from './board-row'
 
 export const Board = () => (
-  <div className="flex h-screen flex-col items-center justify-center">
+  <div className="flex flex-col">
     {Array(3)
       .fill(null)
       .map((_, i) => (
