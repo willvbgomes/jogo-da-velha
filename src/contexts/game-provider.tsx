ca3e5763c996890581f07de7c6e21d5273a6695f
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-10-15 20:49:34 -0300

    add xIsNext and getWinner to the GameContext

--- src/contexts/game-provider.tsx
+++ src/contexts/game-provider.tsx
@@ -1,9 +1,11 @@
 import { createContext, ReactNode } from 'react'
-import { SquareValues, useGameRules } from '../hooks/game-rules'
+import { SquareValues, useGameRules, ValidValue } from '../hooks/game-rules'
 
 type ContextProps = {
   gameOver: boolean
   squares: SquareValues
+  xIsNext: boolean
+  getWinner: (squares: SquareValues) => ValidValue | null
   resetGame: () => void
   setSquareValue: (index: number) => void
 }
@@ -15,13 +17,16 @@ type GameContextProps = {
 export const GameContext = createContext<ContextProps | null>(null)
 
 export const GameProvider = ({ children }: GameContextProps) => {
-  const { gameOver, squares, resetGame, setSquareValue } = useGameRules()
+  const { gameOver, squares, xIsNext, getWinner, resetGame, setSquareValue } =
+    useGameRules()
 
   return (
     <GameContext.Provider
       value={{
         gameOver,
         squares,
+        xIsNext,
+        getWinner,
         resetGame,
         setSquareValue,
       }}
