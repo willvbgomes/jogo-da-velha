75dda312de31e3563da7f37a403c1b0c6816effe
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-10-15 21:00:04 -0300

    update app: add game status

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import { Board } from './components/board'
 import { RestartButton } from './components/restart-button'
+import { Status } from './components/status'
 import { GameProvider } from './contexts/game-provider'
 
 export const App = () => (
   <div className="flex h-screen flex-col items-center justify-center gap-10">
     <GameProvider>
+      <Status />
       <Board />
       <RestartButton />
     </GameProvider>
